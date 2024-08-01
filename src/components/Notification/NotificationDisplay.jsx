de662e51f94bdec20e7151feed551e74ee81ab6a
bosslife19 <wokodavid001@gmail.com>
2024-07-31 23:24:10 -0700

    new features

--- src/components/Notification/NotificationDisplay.jsx
+++ src/components/Notification/NotificationDisplay.jsx
@@ -1,13 +1,27 @@
 import React, {useState} from 'react'
-import { Container} from "react-bootstrap";
+import axios from 'axios'
+import { Container, Card} from "react-bootstrap";
 import NavigationHeader from '../Navigations/NavigationHeader';
 import TitleHeader from '../Headers/TitleHeader';
 import NonAvaliable from '../NonAvaliable/NonAvaliable';
+
 import ConditionalSideNavigation from '../Navigations/ConditionalSideNavigation';
 
 function NotificationDisplay() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const baseUrl = process.env.REACT_APP_EDO_SUBEB_BASE_URL;
+    const [notifications, setNotifications] = useState([])
 
+    const getNotifications = async()=>{
+      try {
+        const res = await axios.get(`${baseUrl}/api/notification`)
+        console.log(res.data)
+        setNotifications(res.data.notifications);
+      } catch (error) {
+        console.log(error)
+      }
+      
+    }
     const toggleSidebar = () => {
       setIsSidebarOpen(!isSidebarOpen);
     };
@@ -18,7 +32,42 @@ function NotificationDisplay() {
     <ConditionalSideNavigation isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <Container className="reportContainer">
         <TitleHeader text={"Notifications"} />
-        <NonAvaliable textMessage={'Sorry, you currently have no notifications, please check back later!'}/>
+        {
+          notifications ?( <Card.Body className="AdminRecentUser m-4 rounded">
+            {notifications.map((log) => (
+              <Row
+                key={log.id}
+                className="align-items-center mb-2 py-2 "
+              >
+                <Col xs={4} lg={2} sm={4} md={4}>
+                  <span className="">{log.title}</span>
+                </Col>
+                <Col xs={4} lg={2} sm={4} md={4}>
+                  <a
+                    href="/"
+                    className="text-decoration-none text-success"
+                  >
+                    {log.body}
+                  </a>
+                </Col>
+               
+                <Col
+                  xs={2}
+                  lg={2}
+                  className="text-muted d-none d-lg-flex"
+                >
+                  {log.created_at}
+                </Col>
+                {/* <Col xs={4} lg={2} sm={4} md={4} className="text-muted">
+                  {log.time}
+                </Col> */}
+              </Row>
+            ))}
+          </Card.Body>) :(
+            <NonAvaliable textMessage={'Sorry, you currently have no notifications, please check back later!'}/>
+          )
+        }
+        
       </Container>
     </div>
   </div>

406ed052dba4a3fe1b6efb5d4c30e20cf28e5125
Woko David <93683530+bosslife19@users.noreply.github.com>
2024-08-13 00:48:08 -0700

    Update MovementLog.jsx

--- src/pages/WareHouseStaff/Inventory/MovementLog.jsx
+++ src/pages/WareHouseStaff/Inventory/MovementLog.jsx
@@ -253,7 +253,7 @@ function MovementLog() {
                               </td>
                             <td>
                               <button onClick={handleReceived(Item.id, 'received','received')} style={{paddingTop:2,
-                                 paddingBottom:2, paddingRight:3, paddingLeft:3,borderRadius:3, backgroundColor:
+                                 paddingBottom:2, paddingRight:3,color:'white',cursor:"pointer", paddingLeft:3,borderRadius:3, backgroundColor:
                                   Item.status=='received'?'green':'#6a5a2c' }} disabled={disable}>{Item.status=='recieved'?'received':'confirm'}</button>
                             </td>
                        </tr>
